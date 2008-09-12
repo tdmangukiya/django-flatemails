586c85f9b8bfe53b0d0dbffa0ab5ff963ddcafad
zbyte64 <zbyte64@ee0bfa4a-8124-11dd-8b6f-79bb0735e950>
2008-09-12 23:59:42 +0000

    initial commit

---
+++ trunk/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+version = '0.1'
+
+setup(name='flatemails',
+      version=version,
+      description="Email and Email template manager for Django",
+      author="Jason Kraus",
+      author_email="zbyte64@gmail.com",
+      url="http://code.google.com/p/django-flatemails/",
+      packages = ['flatemails']
+      )
